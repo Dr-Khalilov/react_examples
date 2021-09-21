be2051034a04e48223f8bbb9eac202dab264a18f
Umar Khalilov <ERMASTER100@gmail.com>
2021-09-21 18:07:19 +0300

    Add new func to Counter

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
-class Counter extends Component {
+export default class Counter extends Component {
     constructor(props) {
         super(props);
         this.state = {
             count: 0,
-            step: 0,
+            step: 5,
+            isAdd: true,
         };
     }
 
@@ -25,18 +26,23 @@ class Counter extends Component {
 
     editStep = ({ target: { value } }) => this.setState({ step: Number(value) });
 
+    changeMode = () => {
+        const { isAdd } = this.state;
+        this.setState({ isAdd: !isAdd });
+    };
+
     render() {
-        const { count, step } = this.state;
+        const { count, step, isAdd } = this.state;
         return (
             <>
                 <h1>Current step: {step}</h1>
                 <input type='number' name='number' value={step} onChange={this.editStep} />
                 <h1>Counter: {count}</h1>
-                <button onClick={this.increment}>ADD</button>
-                <button onClick={this.decrement}>DELETE</button>
+                <button onClick={this.changeMode}>Change Mode</button>
+                &nbsp;
+                {isAdd ? <button onClick={this.increment}>ADD</button> :
+                    <button onClick={this.decrement}>Subtract</button>}
             </>
         );
     }
 }
-
-export default Counter;
